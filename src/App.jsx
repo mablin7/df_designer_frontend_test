72444443001411f5d473faa9614259a1ba4b2ae1
Artem Klementev <o.klmntv@gmail.com>
2022-08-24 19:13:18 +0300

    подключил cytoscape

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,8 @@ import "./App.css"
 import { useState } from "react"
 import Graph from "./components/Graph"
 import GraphDropdown from "./components/Dropdown"
-import cytoscape from 'cytoscape'
-const cy = cytoscape({
-  container: document.getElementById("cy"), // container to render in
-})
+import CytoscapeComponent from "react-cytoscapejs"
+
 function App() {
   const [graph, setGraph] = useState([])
   const handleClick = (value) => {
@@ -23,6 +21,20 @@ function App() {
       <GraphDropdown handleClick={handleClick} />
       <br />
       <Graph graph={graph} />
+      <CytoscapeComponent
+        elements={[
+          { data: { id: "1", label: "Node 1" }, position: { x: 0, y: 0 } },
+          { data: { id: "2", label: "Node 2" }, position: { x: 150, y: 70 } },
+          {
+            data: {
+              source: "1",
+              target: "2",
+              label: "Edge from Node1 to Node2",
+            },
+          },
+        ]}
+        style={{ border: "1px solid black", width: "100%", height: "600px" }}
+      />
     </div>
   )
 }
